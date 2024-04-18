4bb56baa4a53a4fdef37b6cf46e7aec06e772c9c
Silvia España <silviaespanagil@gmail.com>
2024-04-18 17:13:52 +0200

    Create viewModel Add method to startTimer Add method to get the available time string

---
+++ pomodoroTimer/pomodoroTimer Watch App/Timer/TimerViewModel.swift
@@ -0,0 +1,41 @@
+//
+//  TimerViewModel.swift
+//  pomodoroTimer Watch App
+//
+//  Created by Silvia España Gil on 18/4/24.
+//
+
+import Foundation
+
+class TimerViewModel: ObservableObject {
+    
+    @Published var timerValue = 4
+    @Published var isTimerRunning = false
+    @Published var completedCycles = 0
+    @Published var timer: Timer?
+    
+    func getAvailableTimeString(time: Int) -> String {
+        
+        let minutes = time / 60
+        let seconds = time % 60
+        return "\(String(format: "%02d", minutes)):\(String(format: "%02d", seconds))"
+    }
+    
+    func startTimer() {
+        
+        isTimerRunning = true
+        
+        self.timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
+            
+            if self.timerValue > 0 {
+                self.timerValue -= 1
+            } else {
+                
+                self.timer?.invalidate()
+                self.timer = nil
+                self.isTimerRunning = false
+                self.completedCycles += 1
+            }
+        }
+    }
+}
