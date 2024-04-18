c979b8852c8a9d9447f4a9b54c81cf1c9bf04db6
Silvia España <silviaespanagil@gmail.com>
2024-04-18 13:23:11 +0200

    First commit

---
+++ pomodoroTimer/pomodoroTimer Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  pomodoroTimer Watch App
+//
+//  Created by Silvia España Gil on 18/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ pomodoroTimer/pomodoroTimer Watch App/pomodoroTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  pomodoroTimerApp.swift
+//  pomodoroTimer Watch App
+//
+//  Created by Silvia España Gil on 18/4/24.
+//
+
+import SwiftUI
+
+@main
+struct pomodoroTimer_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
